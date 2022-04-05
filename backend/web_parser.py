e79550a728961acdfcc1bd5bffbaf13407e1b3f6
Nathaniel Patanjo <dent@Nathaniels-MacBook-Pro.local>
2022-04-04 22:32:21 -0700

    JSON is working! Check the BACKING_FILE for format

--- backend/web_parser.py
+++ backend/web_parser.py
@@ -3,29 +3,53 @@
 from bs4 import BeautifulSoup
 import requests
 import re
-
-class Golf_Course:
-
-    def __init__(self, name):
-        self.name = name
-
+import json
 
 
 def main():
     html = get_html("https://ncrdb.usga.org/courseTeeInfo.aspx?CourseID=2711")
-    scrape_html(html)
-    pass
+    soup = BeautifulSoup(html.content, 'html.parser')
+    course_obj = create_course_obj(soup)
+    dump_to_json_file(course_obj)
 
+"""
+Creates a new Golf_Course object with the name from the html
+"""
+def create_course_obj(soup):
+    name = soup.table.td.text
+    s = soup.find("table", id="gvTee")
+    content = s.find_all('td')
 
+    arr = []
+
+    for i in content:
+        arr.append(i.text.replace(" ", "").replace("\n", "").replace("\r", ""))
+    i = 0
+    ret_arr = []
+    while i < len(arr)/8:
+        ret_arr.append({
+            "color" : arr[0],
+            "gender" : arr[1],
+            "par" : arr[2],
+            "course_rating_18" : arr[3],
+            "bogey_rating_18" : arr[4],
+            "slope_rating_18" : arr[5],
+            "front_9" : arr[6],
+            "back_9" : arr[7],
+            })
+        arr = arr[8:]
+    ret_map = {}
+    ret_map["course_name"] = name
+    ret_map["course_ratings"] = ret_arr
+    return ret_map
 
 def get_html(url):
-    return requests.get(url).text
-    pass
+    return requests.get(url)
 
-def scrape_html(html):
-    print(html)
-    pass
 
+def dump_to_json_file(course_map):
+    with open("BACKING_FILE", "w") as f:
+        f.write(json.dumps(course_map, indent=2))
 
 
 
