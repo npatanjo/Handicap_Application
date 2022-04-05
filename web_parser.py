591470018fcc3db0b30314dac990e48e2fdaebf9
Nathaniel Patanjo <dent@Nathaniels-MacBook-Pro.local>
2022-04-04 19:44:20 -0700

    added parser file

---
+++ web_parser.py
@@ -0,0 +1,29 @@
+
+
+from bs4 import BeautifulSoup
+import requests
+import re
+
+
+def main():
+    pass
+
+
+
+
+
+
+
+
+
+def get_html(url):
+    pass
+
+def scrape_html(html):
+    pass
+
+
+
+
+if __name__ == "__main__":
+    main()
