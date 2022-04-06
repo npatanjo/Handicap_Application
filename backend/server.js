58049b169217173cfefbeaf7f5743ecd1000d257
Nathaniel Patanjo <dent@Nathaniels-MacBook-Pro.local>
2022-04-05 19:16:59 -0700

    set up heroku

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,17 @@ const app = express();
 
 const { MongoClient } = require("mongodb");
 
-const uri = "mongodb+srv://USERNAME:PASSWORD@CLUSTER_NAME.mongodb.net/DATABASE_NAME?retryWrites=true&w=majority";
+/*
+ * Username is set to <dev> 
+ * PASSWORD is set to <gnu711>
+ * CLUSTER_NAME is set to <handicap>
+ * DATABASE_NAME is set to <handicap>
+ * The name of the heroku app is <handicap-app-711> dont ask me why smh
+ */
+
+// const uri = "mongodb+srv://USERNAME:PASSWORD@CLUSTER_NAME.mongodb.net/DATABASE_NAME?retryWrites=true&w=majority";
+
+const uri = process.env.MONGODB_URI;
 
 // use the express-static middleware
 app.use(express.static("public"));
