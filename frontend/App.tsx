4ddaa5e6bb8f5459e4b317f02b0c42aa4060ed38
Nick Donfris <ndonfris@email.arizona.edu>
2022-05-11 17:57:58 -0700

    App.tsx is now fixed.

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,14 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { LogBox } from 'react-native';
+import { BottomNavigation } from 'react-native-paper';
 import { StyleSheet, Text, View } from 'react-native';
+import SearchCourseScreen from './screens/SearchCourseScreen';
+import SavedCourseScreen from './screens/SavedCourseScreen';
+import AccoutScreen  from './screens/AccountScreen';
+import LoginWrapperScreen from './screens/LoginWrapperScreen';
+import colors from './utilities/Colors';
+//import { MaterialCommunityIcons } from '@expo/vector-icons';
+LogBox.ignoreLogs(["Syntax Error", "JSON Parse error: Unrecognized token"]);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const [index, setIndex] = useState(0);
+
+    /* useState hook, defines values for it's inner items */
+    const [routes] = useState([
+        {key: 'login', title: 'login', icon: 'login', color: colors.darkGreen},
+        {key: 'search', title: 'Search', icon: 'golf-cart', color: colors.darkGreen },
+        {key: 'saved', title: 'Saved', icon: 'golf', color: colors.darkGreen},
+        {key: 'account', title: 'Browse', icon: 'account', color: colors.darkGreen},
+    ]);
+
+    /* maps the state to the actual page route */
+    const renderScene = BottomNavigation.SceneMap({
+        login: LoginWrapperScreen,
+        search: SearchCourseScreen,
+        saved: SavedCourseScreen,
+        account: AccoutScreen,
+    });
+
+    /* return the bottom tabbar component and render the active page (initializes to Search)  */
+    return (
+        <BottomNavigation
+            navigationState={{ index, routes }}
+            onIndexChange={setIndex}
+            renderScene={renderScene}
+            shifting={true}
+            barStyle={{
+                backgroundColor: colors.darkGreen,
+                height: 70,
+            }}
+            inactiveColor={colors.lightGrey}
+            activeColor={colors.white}
+        />
+    );
+};
 
 const styles = StyleSheet.create({
   container: {
